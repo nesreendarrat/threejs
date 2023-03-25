db726f06878782dd588140b4cddcd71a052096dd
nesreendarrat <124673712+nesreendarrat@users.noreply.github.com>
2023-03-26 01:34:45 +0200

    aaded yml file

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/threejs.js/",
+  
   plugins: [react()],
+  base:"/threejs/",
   
 });
 
